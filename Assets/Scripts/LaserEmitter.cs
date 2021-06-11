5af9ff86f4bf64cf57468873c3355a9d77f71130
poetahto <walls.dk@gmail.com>
2021-06-11 16:41:22 -0700

    started lasers

---
+++ Assets/Scripts/LaserEmitter.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class LaserEmitter : MonoBehaviour
+{
+    [SerializeField] 
+    private LineRenderer laserLine;
+
+    private RaycastHit[] _results = new RaycastHit[100];
+    private Vector3[] _laserPositions = new Vector3[2];
+    private LaserReceiver _currentlyActivating;
+    
+    private void Update()
+    {
+        RaycastLaser();
+    }
+
+    private void RaycastLaser()
+    {
+        if (Physics.RaycastNonAlloc(transform.position, transform.forward, _results) > 1)
+        {
+            var closestHit = _results.OrderBy(hit => hit.distance).First(hit => hit.transform != transform);
+
+            _laserPositions[0] = transform.position;
+            _laserPositions[1] = closestHit.point;
+            laserLine.SetPositions(_laserPositions);
+
+            if (closestHit.transform.TryGetComponent<LaserReceiver>(out var receiver))
+            {
+                receiver.SetActive(true);
+                _currentlyActivating = receiver;
+            }
+            else
+            {
+                if (_currentlyActivating)
+                {
+                    _currentlyActivating.SetActive(false);
+                    _currentlyActivating = null;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LaserReceiver.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class LaserReceiver : MonoBehaviour
+{
+    public void SetActive(bool active)
+    {
+        print("set active: " + active);
+    }
+}
