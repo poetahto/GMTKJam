c1dd2558647a64feb91defa2f3afe056b134e077
rain474 <lordraindance5@gmail.com>
2021-06-11 17:12:24 -0400

    renamed and refactored scripts

--- Assets/Scripts/VerticalTransformer.cs
+++ Assets/Scripts/VerticalTransformer.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using NaughtyAttributes;
 
-public class Door : MonoBehaviour
+public class VerticalTransformer : MonoBehaviour
 {
-    public bool test = false;
     public float time = 1f;
     public Ease ease;
     public float defaultVertical;
@@ -17,7 +17,7 @@ public class Door : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.open = false;
+        open = false;
         initialPosition = gameObject.transform.position;
         defaultVertical = gameObject.transform.localScale.y;
     }
@@ -25,48 +25,54 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (test && !isOpen())
-        {
-            OpenDoor();
-        } else if (!test && isOpen())
+
+    }
+
+    [Button]
+    private void TestDoor()
+    {
+        if (IsOpen())
         {
             CloseDoor();
+        } else
+        {
+            OpenDoor();
         }
     }
 
     public void OpenDoor() 
     {
-        if (isOpen()) return;
+        if (IsOpen()) return;
         UpdateOpen(true);
         Vector3 newPosition = initialPosition - new Vector3(0, defaultVertical, 0);
         gameObject.transform
             .DOMove(newPosition, time)
             .SetEase(ease)
-            .OnComplete(fullyUpdated);
+            .OnComplete(FullyUpdated);
     }
 
     public void CloseDoor()
     {
-        if (!isOpen()) return;
+        if (!IsOpen()) return;
         UpdateOpen(false);
         gameObject.transform
             .DOMove(initialPosition, time)
             .SetEase(ease)
-            .OnComplete(fullyUpdated);
+            .OnComplete(FullyUpdated);
     }
 
-    private void UpdateOpen(bool open) 
+    private void UpdateOpen(bool update) 
     {
-        this.open = open;
+        this.open = update;
         //events
     }
 
-    private void fullyUpdated()
+    private void FullyUpdated()
     {
         //more events
     }
 
-    public bool isOpen()
+    public bool IsOpen()
     {
         return open;
     }
