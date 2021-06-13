9d2e034c47ddd89fabcc5427a632da89f43475fa
poetahto <walls.dk@gmail.com>
2021-06-12 22:54:44 -0700

    basic main menu

---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using DG.Tweening;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelButton : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup group;
+    [SerializeField] private float fadeTime = 1f;
+    [SerializeField] private float hoverScale = 1.2f;
+    [SerializeField] private float hoverTime = 0.25f;
+    [Scene, SerializeField] private string targetScene;
+
+    private Tweener _hoverAnimation;
+    
+    private void Awake()
+    {
+        group.interactable = false;
+        group.blocksRaycasts = false;
+        group.alpha = 0f;
+
+        _hoverAnimation = transform.DOScale(hoverScale, 1 / hoverTime)
+            .SetSpeedBased()
+            .SetUpdate(true)
+            .SetAutoKill(false)
+            .Pause();
+    }
+
+    private void OnDestroy()
+    {
+        _hoverAnimation.Kill();
+    }
+
+    public void Enable()
+    {
+        group.interactable = true;
+        group.blocksRaycasts = true;
+        group.DOFade(1, fadeTime);
+    }
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(targetScene);
+    }
+
+    public void OnEnter()
+    {
+        _hoverAnimation.PlayForward();
+    }
+
+    public void OnExit()
+    {
+        _hoverAnimation.PlayBackwards();
+    }
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup titleText;
+    [SerializeField] private Image background;
+    [SerializeField] private Color startColor;
+    [SerializeField] private Color endColor;
+    [SerializeField] private float colorShiftTime;
+    
+    [SerializeField] private LevelButton[] levels;
+    
+    [SerializeField] private float introDuration = 1f;
+    [SerializeField] private float introHangTime = 1f;
+    [SerializeField] private Vector3 postIntroTitleLocation;
+    [SerializeField] private float postIntroTitleScale;
+    [SerializeField] private float postIntroTransitionDuration;
+    [SerializeField] private float levelAppearTime = 0.1f;
+    
+    private IEnumerator Start()
+    {
+        background.color = startColor;
+        background.DOColor(endColor, colorShiftTime).SetLoops(-1, LoopType.Yoyo);
+        
+        titleText.alpha = 0;
+        titleText.DOFade(1, introDuration);
+        yield return new WaitForSeconds(introDuration + introHangTime);
+
+        titleText.transform.DOScale(postIntroTitleScale, postIntroTransitionDuration);
+        titleText.transform.DOLocalMove(postIntroTitleLocation, postIntroTransitionDuration);
+        yield return new WaitForSeconds(postIntroTransitionDuration);
+        
+        foreach (var levelButton in levels)
+        {
+            levelButton.Enable();
+            yield return new WaitForSeconds(levelAppearTime);
+        }
+    }
+}
